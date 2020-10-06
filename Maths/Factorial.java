fdd455cb8d0582fa391831834422a47da46cf1e8
A Shiv <44468979+ShivayStark@users.noreply.github.com>
2020-10-06 21:37:59 +0530

    Update Factorial.java

--- Maths/Factorial.java
+++ Maths/Factorial.java
@@ -1,12 +1,8 @@
-package Maths;
-
 public class Factorial {
     public static void main(String[] args) {
         int n = 5;
         System.out.println(n + "! = " + factorial(n));
-    }
-
-    /**
+    } /**
      * Calculate factorial
      *
      * @param n the number
@@ -15,11 +11,8 @@ public class Factorial {
     public static long factorial(int n) {
         if (n < 0) {
             throw new ArithmeticException("n < 0");
-        }
-        long fac = 1;
+        } long fac = 1;
         for (int i = 1; i <= n; ++i) {
             fac *= i;
-        }
-        return fac;
-    }
-}
+        }return fac;
+    }}
